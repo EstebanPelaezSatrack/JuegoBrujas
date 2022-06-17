bde99a355481415f0b853b83de6a0cf3778e2d1d
Esteban Pelaez Florez <jhon_pelaez@satrack.com>
2022-06-17 12:14:26 -0500

    Se crea clase de trayectos para el juego

---
+++ JuegoBrujas.BL/Trayecto.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace JuegoBrujas.BL
+{
+    public class Trayecto
+    {
+        public Trayecto()
+        {
+
+        }
+        public Trayecto(int trayectoId)
+        {
+            TrayectoId = trayectoId;
+        }
+        public int TrayectoId
+        {
+            get;
+            private set;
+        }
+
+        public Pueblo PuebloActual
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public Pueblo PuebloDestino
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public double Distancia
+        {
+            get=> default;
+            set
+            {
+            }
+        }
+
+        public double Duracion
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public int Compra
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public bool ValidarReservaPueblo(Pueblo pueblo)
+        {
+            var iDisponible = true;
+
+            if (pueblo.InsumosDisponibles<4) iDisponible =false;
+            
+            return iDisponible;
+            
+        }
+
+     
+        public void Comprar(Pueblo pueblo,Viaje viaje)
+        {
+            int DisponibleCompraPueblo = pueblo.InsumosDisponibles - 3;
+
+            if (DisponibleCompraPueblo > viaje.ObjetivoCompra)
+            {
+                Compra = viaje.ObjetivoCompra;
+            }
+            else
+            {
+                Compra = DisponibleCompraPueblo;
+            }               
+        }
+
+        //La distancia se da en Kilometros
+        public void CalcularDistancia()
+        {
+            double distancia = Math.Sqrt((Math.Pow(PuebloActual.PosicionEnX - PuebloDestino.PosicionEnX,2)+Math.Pow(PuebloActual.PosicionEnY-PuebloDestino.PosicionEnY,2)));
+            this.Distancia=distancia;
+                                                    
+        }
+
+        /*La duración se da en Horas*/
+        public void ValidarDuracion(Escoba escoba)
+        {
+            double duracion = Distancia / escoba.Velocidad;
+            this.Duracion = duracion;                                  
+        }   
+        
+    }
+}
