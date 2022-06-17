bf3f7d2bf256b3a206cb393835091ea6a8eeafa2
Esteban Pelaez Florez <jhon_pelaez@satrack.com>
2022-06-17 17:40:40 -0500

    Se crea clase de vecinos para el juego

---
+++ JuegoBrujas.BL/Vecinos.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace JuegoBrujas.BL
+{
+    public class Destino
+    {
+        public int Distancia
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public string Nombre
+        {
+            get => default;
+            set
+            {
+            }
+        }
+    }
+}
