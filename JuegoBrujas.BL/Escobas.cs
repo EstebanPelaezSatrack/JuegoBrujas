7b8a353fb6594356b175d13144cbe8ba408b5997
Esteban Pelaez Florez <jhon_pelaez@satrack.com>
2022-06-17 17:46:41 -0500

    Se crea clase de escobas para el juego

---
+++ JuegoBrujas.BL/Escobas.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace JuegoBrujas.BL
+{
+    public class Escoba
+    {
+        public Escoba()
+        {
+
+        }
+        public Escoba(int escobaId)
+        {
+            EscobaId = escobaId;
+        }
+
+        public int EscobaId
+        {
+            get;
+            private set;
+        }
+        public int CapacidadCarga
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        //La velocidad se da en Kilometros/Hora
+        public int Velocidad
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+       
+        public bool ValidarDatos()
+        {
+            var isValid = true;
+            if (CapacidadCarga == 0) isValid = false;
+            if (Velocidad == 0) isValid = false;
+
+            return isValid;
+        }
+    }
+}
